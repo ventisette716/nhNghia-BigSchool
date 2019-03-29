8b728ea30513ec58eb736d9874bf1dc6120d9d44
ventisette716 <lop9a9.nguyenhuunghia@gmail.com>
2019-03-29 10:33:39 +0700

    CREATE COURSES

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,33 @@
+﻿using BigSchool.Models;
+using BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId=viewModel.Category,
+                Place=viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ BigSchool/Models/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace BigSchool.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public ApplicationUser Lecturer { get; set; }
+        [Required]
+        public string LecturerId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+        public DateTime DateTime { get; set; }
+        public Category Category { get; set; }
+        [Required]
+        public byte CategoryId { get; set; }
+    }
+    
+}
